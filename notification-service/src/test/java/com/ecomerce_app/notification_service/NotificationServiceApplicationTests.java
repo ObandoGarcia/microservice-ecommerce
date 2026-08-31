00d6d8152a96b168cbb1408e09f888db8908afd6
Jose Manuel Obando <obandogarcia10635@gmail.com>
2026-08-30 21:32:41 -0600

    Initial commit

---
+++ notification-service/src/test/java/com/ecomerce_app/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce_app.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
